f23829f7ce0c1c8c52e97da9a7768f243c716071
Tom Schof <tom-schof@users.noreply.github.com>
2018-09-19 13:14:56 -0400

    used simpler example for chat

--- routing/handleDisconnect.js
+++
@@ -1,6 +0,0 @@
-function handleDisconnect() {
-    // remove user profile
-    clientManager.removeClient(client)
-    // remove member from all chatrooms
-    chatroomManager.removeClient(client)
-  }
