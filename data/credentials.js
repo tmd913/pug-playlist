011046e3d0b0fcfaa64393764bef6de15e297eb3
Tom Dennis <tdennis913@gmail.com>
2018-09-20 12:39:41 -0400

    added data for credentials

---
+++ data/credentials.js
@@ -0,0 +1,8 @@
+const credentials = [
+    {
+        "accessToken": 'BQAM9d_G7o7enQBFKnFy4vJU0ocI0Uj6TEukwFB06vXVX-WL5ZS1nM6Z25PFA0SU6qT9NalkwKrsvwXoRLv0VApwX28nM5Oeme-limSYb7RlCugsPlhNAd3mU6wAjBQ6wPmiakW01-k8KlvGixamg61UftjpEKwJFy8kby_w4jpWmX11DIbkyY90oyKCOp2VP9R4sdyVIKYsON5VLLwWVmFwKOnKE-1rAthitlAxHopL8lbSpXYUXlhj9-AC2Z15N50P80gAaA9-zcniM20a_w769PI',
+        "refreshToken": 'AQCTP1S90Oknra5YWuc8n-t9ihTOvu-sUN71L2ncfYXfBQEo-0IAv5a81YpvrrCocftTHT3rTZxSLodFwLQGSpYGuh6iG66ZsCQee34kO-spdoXXmPo24GDN11uO1oWgpVC0Ng'
+    }
+]
+
+module.exports = credentials;
